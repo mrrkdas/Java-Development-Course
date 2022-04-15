388c7e74532f76d65daa8744e54732ac7bd65ce2
Rishab Das <das.rishabk@gmail.com>
2022-04-15 14:22:19 -0400

    finished lessons

--- SectionTwo/Bus.java
+++ SectionTwo/Bus.java
@@ -4,17 +4,23 @@ package SectionTwo;
 public class Bus {
     public static void main(String[] args) {
         int passengers = 0;
+        String sentence = "Number of passengers: ";
         passengers = passengers + 9; // You can replace with passengers += 9
 
-        System.out.println(passengers);
+        System.out.println(sentence + passengers);
 
         passengers = passengers - 5; // You can replace with passengers -= 5
 
-        System.out.println(passengers);
+        System.out.println(sentence + passengers);
 
         passengers = passengers - 4; // You can replace with passengers -= 4
 
-        System.out.println(passengers);
+        System.out.println(sentence + passengers);
+
+        // Updating the string variables
+        sentence = "end of shift";
+
+        System.out.println(sentence);
     }
     
 }
