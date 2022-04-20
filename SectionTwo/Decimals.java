643c52ff1eb9add0e788999d0804f1e6d04dd93e
Rishab Das <das.rishabk@gmail.com>
2022-04-19 21:22:05 -0400

    learned about double

---
+++ SectionTwo/Decimals.java
@@ -0,0 +1,15 @@
+package SectionTwo;
+
+public class Decimals {
+    public static void main(String[] args) {
+        double percentage = 55.7;
+        System.out.println("In 2020 " + percentage + "% of the world's pop is urban");
+        
+        double dividend = 25;
+        double divisor = 2;
+        // If you divide 2 integers, it will be an integer, always use double in math calculations
+        System.out.println(dividend/divisor);
+    
+    }
+
+}
