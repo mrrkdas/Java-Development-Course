8fba5b626a5b02712d961d5aca4c1108c751f6eb
Rishab Das <das.rishabk@gmail.com>
2022-04-19 21:15:48 -0400

    learned about int and long

---
+++ SectionTwo/WholeNumbers.java
@@ -0,0 +1,15 @@
+package SectionTwo;
+
+public class WholeNumbers{
+    public static void main(String[] args) {
+        int numberOfFans = 5000;
+        System.out.println("There are " + numberOfFans + " at the Quidditch Games");
+        
+        // Int can't hold values greater than 2 billion
+        long globalPopulation = 7000000000L;
+        System.out.println("There are " + globalPopulation + " people on earth");
+
+        long dailyGoogleSearches = 50000000L;
+        System.out.println("There are " + dailyGoogleSearches + " google searches better");
+    }
+}
