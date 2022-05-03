b5d91954470b0a133ced412abe97f503bf360538
Rishab Das <das.rishabk@gmail.com>
2022-05-03 12:13:24 -0400

    finished with adding different types

--- SectionTwo/MathOperations.java
+++ SectionTwo/MathOperations.java
@@ -1,6 +1,33 @@
 package SectionTwo;
 
 public class MathOperations {
+    public static void main(String[] args) {
+        int bagOfSweets1 = 5;
+        int bagOfSweets2 = 10;
+
+        long starsInMilyWay = 250000000L;
+        long starsInAnromeda = 10000000000000L;
+
+        double testScore = 6.7;
+        double bonusMarks = 2.5;
+
+        System.out.println(bagOfSweets1 + bagOfSweets2);
+        System.out.println(starsInAnromeda + starsInMilyWay);
+        System.out.println(testScore + bonusMarks);
+    }
     
     
 }
+
+
+// Fred and George collected <result> sweets
+// There are <result> stars in the Milky Way and Andromeda galaxies.
+// With bonus marks, Ron scored <result>/10 on his potions test. Snape wasn't too thrilled
+// Fred has  <result>  more sweets than George
+// The Andromeda Galaxy has <result> more stars than the Milky Way
+// Snape ended up removing marks. Ron actually scored <result> on his potions test
+// Fred and George used a special spell to multiply their sweets to: <result>
+// Snape made an error on his excel, accidentally multiplying Ron's score to: <result>/10
+// Originally, Fred had  <result> times more sweets than George
+// Andromeda has <result> times more stars
+// Snape caught the error. Furious, he divided Ron's score to: <result>
