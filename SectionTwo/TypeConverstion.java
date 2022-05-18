811457e87b4e3056b340bfc847af159dad42162c
Rishab Das <das.rishabk@gmail.com>
2022-05-17 21:25:07 -0400

    type casting

---
+++ SectionTwo/TypeConverstion.java
@@ -0,0 +1,11 @@
+package SectionTwo;
+public class TypeConverstion {
+    public static void main(String[] args) {
+        double salary = 5833.3333;
+        System.out.println("Percy's monthly salary is $" + salary);
+
+        int rounded_salary = (int)salary;
+        System.out.println("Percy's monthly salary is $" + rounded_salary);
+    }
+    
+}
