f511bd1b67c76e4891a0b9b8eedb677875644633
Rishab Das <das.rishabk@gmail.com>
2022-06-03 21:14:47 -0400

    comparisons

---
+++ Section3/BooleansComparisons.java
@@ -0,0 +1,24 @@
+package Section3;
+
+public class BooleansComparisons{
+    public static void main(String[] args) {
+        int chemistryGrade = 95;
+        int biologyGrade = 75;
+        int englishGrade = 75;
+
+        System.out.println(chemistryGrade != englishGrade);
+        System.out.println(englishGrade != biologyGrade);
+
+        String sentence = "I love this course";
+        String sentence2 = "I love this course";
+
+        System.out.println(!sentence.equals(sentence2));
+
+        // double sales = 35.55;
+        /// double costs = 5.55;
+
+        // System.out.println(sales <  costs);
+
+
+    }
+}
