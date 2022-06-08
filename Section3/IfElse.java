72d942051056c9b8dc29ff28ffac1f97dae04637
Rishab Das <das.rishabk@gmail.com>
2022-06-07 21:17:57 -0400

    if else statemnts

---
+++ Section3/IfElse.java
@@ -0,0 +1,14 @@
+package Section3;
+
+public class IfElse {
+    public static void main(String[] args) {
+        int Grade = 50;
+
+        if (Grade >= 50) {
+            System.out.println("Great Job, you pass!!");
+        } else {
+            System.out.println("Study hard next time");
+        }
+        
+    }
+}
