98f1034d0fd5a9901e1bbf69b3fb5077e0b417d8
Rishab Das <das.rishabk@gmail.com>
2022-06-09 10:54:37 -0400

    if elseif else

--- Section3/IfElse.java
+++ Section3/IfElse.java
@@ -2,11 +2,19 @@ package Section3;
 
 public class IfElse {
     public static void main(String[] args) {
-        int Grade = 50;
+        int Grade = 65;
 
-        if (Grade >= 50) {
-            System.out.println("Great Job, you pass!!");
-        } else {
+        if (Grade >= 80) {
+            System.out.println("You got an A");
+        } else if (Grade >= 70) {
+            System.out.println("You got a B");
+        } else if (Grade >= 60) {
+            System.out.println("You got a C");
+        } else if (Grade > 50) {
+            System.out.println("You got a D");
+        }
+        
+        else {
             System.out.println("Study hard next time");
         }
         
