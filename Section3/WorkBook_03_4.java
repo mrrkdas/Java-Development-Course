67e70b5ec7c06c632f7b2c46008e3d52e0119199
Rishab Das <das.rishabk@gmail.com>
2022-06-12 11:27:05 -0400

    finished workbook 3.4

---
+++ Section3/WorkBook_03_4.java
@@ -0,0 +1,18 @@
+package Section3;
+
+public class WorkBook_03_4 {
+    public static void main(String[] args) {
+        int temp = 25;  
+
+        String forecast;
+        if (temp > -15 && temp < -1) {
+            System.out.println("The forecast is FREEZING! Stay home!");
+        } else if (temp > 0 && temp < 10) {
+            System.out.println("The forecast is Chilly. Wear a coat!");
+        } else {
+            System.out.println("It's warm, go outside!!");
+        }
+        System.out.println(forecast);
+    }
+    
+}
