727d1b9b07396651721fbc09a2ed51daae49c6f7
Rishab Das <das.rishabk@gmail.com>
2022-06-16 15:23:50 -0400

    finished workbook 3.6

--- Section3/WorkBook_03_6.java
+++ Section3/WorkBook_03_6.java
@@ -11,13 +11,13 @@ public class WorkBook_03_6 {
         System.out.println("Hmm, let me check my calendar.");
 
         switch (day){
-            case "Monday": System.out.println("I have to work late");
-            case "Tuesday": System.out.println("I have a meeting");
-            case "Wednesday": System.out.println("I have to stay late");
-            case "Thursday": System.out.println("I have to stay late");
-            case "Friday": System.out.println("I'm free!!!");
-            case "Saturday": System.out.println("I'm Free!!!");
-            case "Sunday" : System.out.println("I'm Free!!!");
+            case "Monday": System.out.println("I have to work late"); break;
+            case "Tuesday": System.out.println("I have a meeting"); break;
+            case "Wednesday": System.out.println("I have to stay late"); break;
+            case "Thursday": System.out.println("I have to stay late"); break;
+            case "Friday": System.out.println("I'm free!!!"); break;
+            case "Saturday": System.out.println("I'm Free!!!"); break;
+            case "Sunday" : System.out.println("I'm Free!!!"); break;
             default: System.out.println("That's not a real day");
         }
     }
