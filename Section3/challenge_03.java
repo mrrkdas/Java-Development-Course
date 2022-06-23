01eb31626ab3419e65102cf9326f81e6c75f8a8e
Rishab Das <das.rishabk@gmail.com>
2022-06-23 13:40:22 -0400

    added switch statements

--- Section3/challenge_03.java
+++ Section3/challenge_03.java
@@ -6,24 +6,53 @@ public class challenge_03 {
 
         System.out.println("1. Which country held the 2016 Summer Olympics?");
         System.out.println("\ta) China\n\tb) Ireland\n\tc) Brazil\n\td) Italy\n");
-        //store answer 1
+        String answer1 = scan.nextLine();
 
         System.out.println("\n2. Which planet is the hottest?");
         System.out.println("\ta) Venus\n\tb) Saturn\n\tc) Mercury\n\td) Mars\n");
-        //store answer 2
+        String answer2 = scan.nextLine();
 
         System.out.println("\n3. What is the rarest blood type?");
         System.out.println("\ta) O\n\tb) A\n\tc) B\n\td) AB-Negative\n");
-        //store answer 3
+        String answer3 = scan.nextLine();
 
         System.out.println("\n4. Which one of these characters is friends with Harry Potter?");
         System.out.println("\ta) Ron Weasley\n\tb) Hermione Granger\n\tc) Draco Malfoy\n");
-        //store answer 4
+        String answer4 = scan.nextLine();
 
         int score = 0;
 
 
-        //Task 2: Check each answer - For each correct answer, add 5 points to the score. 
+        switch (answer1) {
+            case "a" : System.out.println("Wrong Answer"); break;
+            case "b" : System.out.println("Wrong Answer"); break;
+            case "c" : score += 5; break; 
+            case "d" : System.out.println("Wrong Answer"); break;
+            default : System.out.println("Thats not a valid answer");
+        }
+
+        switch (answer2) {
+            case "a" : score += 5; break; 
+            case "b" : System.out.println("Wrong Answer"); break;
+            case "c" : System.out.println("Wrong Answer"); break;
+            case "d" : System.out.println("Wrong Answer"); break;
+            default : System.out.println("Thats not a valid answer");
+        }
+
+        switch (answer3) {
+            case "a" : System.out.println("Wrong Answer"); break;
+            case "b" : System.out.println("Wrong Answer"); break;
+            case "c" : System.out.println("Wrong Answer"); break;
+            case "d" : score += 5; break; 
+            default : System.out.println("Thats not a valid answer");
+        }
+
+        switch (answer4) {
+            case "a" : System.out.println("Wrong Answer"); break;
+            case "b" : score += 5; break; 
+            case "c" : System.out.println("Wrong Answer"); break;
+            default : System.out.println("Thats not a valid answer");
+        }
         
         System.out.println("Your final score is: " + score + "/20");
 
