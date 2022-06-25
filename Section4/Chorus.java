4f9bb467f76b1a2bba90f8b7bda8ac43b3448911
Rishab Das <das.rishabk@gmail.com>
2022-06-25 13:38:36 -0400

    ran file

---
+++ Section4/Chorus.java
@@ -0,0 +1,32 @@
+package Section4;
+
+public class Chorus {
+    public static void main(String[] args) {
+        System.out.println("Don't blame it on sunhine");
+        System.out.println("Don't blame it on the moon light");
+        System.out.println("Don't blame it on good times");
+        System.out.println("Blame it on the boogie\n");
+
+        System.out.println("Don't blame it on sunhine");
+        System.out.println("Don't blame it on the moon light");
+        System.out.println("Don't blame it on good times");
+        System.out.println("Blame it on the boogie\n");
+
+        System.out.println("Don't blame it on sunhine");
+        System.out.println("Don't blame it on the moon light");
+        System.out.println("Don't blame it on good times");
+        System.out.println("Blame it on the boogie\n");
+
+        System.out.println("Don't blame it on sunhine");
+        System.out.println("Don't blame it on the moon light");
+        System.out.println("Don't blame it on good times");
+        System.out.println("Blame it on the boogie\n");
+
+        System.out.println("Don't blame it on sunhine");
+        System.out.println("Don't blame it on the moon light");
+        System.out.println("Don't blame it on good times");
+        System.out.println("Blame it on the boogie\n");
+
+        
+    }
+}
