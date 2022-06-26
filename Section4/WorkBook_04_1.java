af8b76ea96a3c94640db4bdca26248c6a9c64119
Rishab Das <das.rishabk@gmail.com>
2022-06-26 14:07:31 -0400

    ran and finished

--- Section4/WorkBook_04_1.java
+++ Section4/WorkBook_04_1.java
@@ -1,5 +1,38 @@
 package Section4;
 
 public class WorkBook_04_1 {
-    
-}
+    public static void main(String[] args) {
+        //what Bart did so far
+        System.out.println("I will not copy and paste code.");
+        System.out.println("I will use Camel Case when writing class names.");
+        System.out.println("I will use lower Camel Case when writing function names.");
+        System.out.println("I will use lower Camel Case when writing variables names.\n");
+
+        bartDetention();
+        bartDetention();
+        bartDetention();
+        bartDetention();
+        bartDetention();
+        bartDetention();
+        
+   }
+
+
+
+    //Task 1 - Make a function here. See the doc comment for details. 
+
+    /**    
+      * Funtion name: printLines
+      *
+      * Inside the function:
+      *   1. prints the four lines       
+      */
+
+      public static void bartDetention() { 
+        System.out.println("I will not copy and paste code.");
+        System.out.println("I will use Camel Case when writing class names.");
+        System.out.println("I will use lower Camel Case when writing function names.");
+        System.out.println("I will use lower Camel Case when writing variables names.\n");
+      }
+    }
+
