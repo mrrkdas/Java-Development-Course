f071b3ac119d42acaa24bb56a4117f04194226c9
Rishab Das <das.rishabk@gmail.com>
2022-06-27 15:09:31 -0400

    added parameters to function

---
+++ Section4/Parameters.java
@@ -0,0 +1,14 @@
+package Section4;
+
+public class Parameters {
+    public static void main(String[] args) {
+        measureRectangle(2.2, 4.3);
+        
+    }
+
+    public static void measureRectangle(double length, double width) {
+        double area = length * width;
+
+        System.out.println("The area of the rectangle is " + area);
+    }
+}
