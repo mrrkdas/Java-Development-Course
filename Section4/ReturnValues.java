e6e5f53d129ceeacf0d5f659f7169181fad43365
Rishab Das <das.rishabk@gmail.com>
2022-07-01 16:02:39 -0400

    added the variables to store

--- Section4/ReturnValues.java
+++ Section4/ReturnValues.java
@@ -2,11 +2,11 @@ package Section4;
 
 public class ReturnValues {
     public static void main(String[] args) {
-        measureRectangle(4.3, 2.2);
-        measureRectangle(3.2, 4.1);
-        measureRectangle(3.5, 1.2);
-        measureRectangle(10.1, 0.7);
-        measureRectangle(20.2, 5.6);
+        double measure1 = measureRectangle(4.3, 2.2);
+        double measure2 = measureRectangle(3.2, 4.1);
+        double measure3 = measureRectangle(3.5, 1.2);
+        double measure4 = measureRectangle(10.1, 0.7);
+        double measure5 = measureRectangle(20.2, 5.6);
         
     }
 
