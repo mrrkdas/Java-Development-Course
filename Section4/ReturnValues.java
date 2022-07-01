2848ea2cef9df3358a3f980cc277ca151df850b3
Rishab Das <das.rishabk@gmail.com>
2022-07-01 16:15:17 -0400

    finished lesson

--- Section4/ReturnValues.java
+++ Section4/ReturnValues.java
@@ -5,19 +5,31 @@ public class ReturnValues {
         double measure1 = measureRectangle(4.3, 2.2);
         double measure2 = measureRectangle(3.2, 4.1);
         double measure3 = measureRectangle(3.5, 1.2);
-        double measure4 = measureRectangle(10.1, 0.7);
-        double measure5 = measureRectangle(20.2, 5.6);
+        double measure4 = measureRectangle(1.6, 5.6);
+        double measure5 = measureRectangle(10.1, 0.7);
+        double measure6 = measureRectangle(20.2, 5.6);
         
+        stringPrinter(4.3, 2.2, measure1);
+        stringPrinter(3.2, 4.1, measure2);
+        stringPrinter(3.5, 1.2, measure3);
+        stringPrinter(1.6, 5.6, measure4);
+        stringPrinter(10.1, 0.7, measure5);
+        stringPrinter(20.2, 5.6, measure6);
+
     }
 
-    System.out.println("The area of a rectangle with the length" + 
-    4.3 + " and length of" + 2.2 + "has an area of " + measure1);
+    
 
     
     public static double measureRectangle(double length, double width) {
         double area = length * width;
 
         return area;
+    }
+
+    public static void stringPrinter(double length, double width, double area) {
+        System.out.println("The area of a rectangle with the length" + 
+        length + " and length of" + width + "has an area of " + area);
     }
     
 }
