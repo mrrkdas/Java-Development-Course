a75c3e47c204fd94808ad7579f525299eeb95465
Rishab Das <das.rishabk@gmail.com>
2022-07-04 13:53:02 -0400

    added first doc comment

--- Section4/DocComments.java
+++ Section4/DocComments.java
@@ -4,4 +4,16 @@ public class DocComments {
     public static void main(String[] args) {
         
     }
+
+    /** 
+     * Function name: greet
+     * 
+     * Inside the function"
+     * 1. prints: "hi"
+     * 
+     */
+
+    public static void greet() {
+        System.out.println("Hi");
+    }
 }
