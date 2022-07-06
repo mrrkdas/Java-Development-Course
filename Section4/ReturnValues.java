a32827dd6f295627eac738a9b8ed0a8dee02116d
Rishab Das <das.rishabk@gmail.com>
2022-07-06 11:57:54 -0400

    fixed code

--- Section4/ReturnValues.java
+++ Section4/ReturnValues.java
@@ -1,45 +1,40 @@
+
 package Section4;
+
+import java.util.Scanner;
 
 public class ReturnValues {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        
         double measure1 = measureRectangle(4.3, 2.2, "area");
-        double measure2 = measureRectangle(3.2, 4.1);
-        double measure3 = measureRectangle(3.5, 1.2);
-        double measure4 = measureRectangle(1.6, 5.6);
-        double measure5 = measureRectangle(10.1, 0.7);
-        double measure6 = measureRectangle(20.2, 5.6);
+        double measure2 = measureRectangle(3.2, 4.1, "perimeter");
         
-        stringPrinter(4.3, 2.2, measure1);
-        stringPrinter(3.2, 4.1, measure2);
-        stringPrinter(3.5, 1.2, measure3);
-        stringPrinter(1.6, 5.6, measure4);
-        stringPrinter(10.1, 0.7, measure5);
-        stringPrinter(20.2, 5.6, measure6);
+        stringPrinter(4.3, 2.2, measure1, "area");
+        stringPrinter(3.2, 4.1, measure2, "perimeter");
 
+        
+        scan.close();
     }
 
-    
+    public static double measureRectangle(double length, double width, String option){
+        if (length < 0 || width < 0) {
+            System.out.println("length or width cannot be negative");
+            System.exit(0);
+        }
 
-    
-    public static double measureRectangle(double length, double width, String option) {
         switch (option) {
-            case "area": return length * width;
-            case "perimeter" : return  2 * (length + width);
+            case "area": return length * width; 
+            case "perimeter": return 2 * (length + width);
             default: return 404;
-        }
-        
 
+        }
     }
 
-    public static void stringPrinter(double length, double width, double area, String option) {
-
-        if (option.equals("area") {
-            System.out.println("The area of a rectangle with the length" + 
-            length + " and length of" + width + "has an area of " + area);
-        } else {
-            System.out.println("The area of a rectangle with the length" + 
-            length + " and length of" + width + "has a perimeter of " + perimeter);
-        }
+    public static void stringPrinter(double length, double width, double measure, String option){
+        System.out.println("The " + option + " of a rectangle with length " + length + 
+        " and with width " + width + " is equal to " + measure + "\n");
 
         
     }
---
+++ Section4/WorkBook_04_4.java
@@ -0,0 +1,7 @@
+package Section4;
+
+public class WorkBook_04_4 {
+    public static void main(String[] args) {
+        
+    }
+}
