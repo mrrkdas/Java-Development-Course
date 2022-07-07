d3dbd245ad910a60307cc30503aca5949ebdcf6f
Rishab Das <das.rishabk@gmail.com>
2022-07-07 14:02:36 -0400

    will finished lesson tmr

--- Section4/BuiltInFunctions.java
+++ Section4/BuiltInFunctions.java
@@ -2,6 +2,10 @@ package Section4;
 public class BuiltInFunctions {
     public static void main(String[] args) {
         System.out.println("Hello Java");
+
+        double sine = Math.sin(1.2);
+
+        System.out.println("The sine of 1.2 is: " + sine);
     }
     
 }
