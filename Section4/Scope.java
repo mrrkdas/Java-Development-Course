f26e2fab17d5175044594cb4a759b965f354b9f6
Rishab Das <das.rishabk@gmail.com>
2022-07-07 13:50:03 -0400

    understanding scope

--- Section4/Scope.java
+++ Section4/Scope.java
@@ -1,7 +1,12 @@
 package Section4;
 public class Scope {
     public static void main(String[] args) {
-        
+        int apples = 5;
+        someFunction();
     }
+    public static void someFunction(){
+        System.out.println(apples);
+    }
+    
     
 }
