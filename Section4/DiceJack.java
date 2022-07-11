ebaa47ec7d74f81d81ec433babf2eb2dba5946ab
Rishab Das <das.rishabk@gmail.com>
2022-07-11 11:37:29 -0400

    created rollDice function

--- Section4/DiceJack.java
+++ Section4/DiceJack.java
@@ -2,8 +2,17 @@ package Section4;
 
 public class DiceJack {
     public static void main(String[] args) {
-        
+        int roll1 = rollDice();
+        int roll2 = rollDice();
+        int roll3 = rollDice();
+
+        System.out.println(roll1);
+        System.out.println(roll2);
+        System.out.println(roll3);
     }
 
-    
+    public static int rollDice() {
+        double randomNumber = Math.random() * 6;
+        return (int) randomNumber;
+    }
 }
