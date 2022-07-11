db6717fe3808f28f5d009e77bc82f49fcb1466e9
Rishab Das <das.rishabk@gmail.com>
2022-07-11 11:37:51 -0400

    fixed problem

--- Section4/DiceJack.java
+++ Section4/DiceJack.java
@@ -13,6 +13,7 @@ public class DiceJack {
 
     public static int rollDice() {
         double randomNumber = Math.random() * 6;
+        randomNumber += 1;
         return (int) randomNumber;
     }
 }
