69df0c8d1ab72f80e28ae3d3f39b1a978fb9b4a6
Rishab Das <das.rishabk@gmail.com>
2022-07-15 11:24:41 -0400

    added first for loop

--- Section5/ForLoops.java
+++ Section5/ForLoops.java
@@ -2,6 +2,8 @@ package Section5;
 
 public class ForLoops {
     public static void main(String[] args) {
-        
+        for (int i =1; i <= 3; i++) {
+            System.out.println(i);
+        }
     }
 }
