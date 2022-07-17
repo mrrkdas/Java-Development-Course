a31d2903d160825720e8e71759dbbaa9b9cebcf2
Rishab Das <das.rishabk@gmail.com>
2022-07-17 15:25:51 -0400

    created file and outline

---
+++ Section5/WorkBook_05_2.java
@@ -0,0 +1,16 @@
+package Section5;
+
+import java.util.Scanner;
+
+public class WorkBook_05_2 {
+    public static void main(String[] args) {
+
+        System.out.println("Hi Bart. I can write lines for you.\nWhat do you want me to write?");
+        Scanner scan = new Scanner(System.in);
+
+        
+        
+    }
+    
+    
+}
