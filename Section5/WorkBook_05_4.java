d5f533e46a28a8ac0f7085ba444f9f4f7aa02dd1
Rishab Das <das.rishabk@gmail.com>
2022-07-19 15:13:08 -0400

    finished with help from DrDabit

--- Section5/WorkBook_05_4.java
+++ Section5/WorkBook_05_4.java
@@ -2,7 +2,10 @@ package Section5;
 
 public class WorkBook_05_4 {
     public static void main(String[] args) {
-           // Task 2: make a for loop that starts at i = 99, and finishes at i=1.
+        // Task 2: make a for loop that starts at i = 99, and finishes at i=1.
+        for (int i = 99; i > 0; i--) {
+            sing(i);
+        }
     }
 
     /** Task 1
@@ -15,5 +18,12 @@ public class WorkBook_05_4 {
      *      <i> bottles of beer on the wall, <i> bottles of beer!
      *      take one down, pass it around, <i-1> bottles of beer on the wall!
      */
-    
+
+     public static void sing(int i) {
+            System.out.println(i  + " bottles of beer on the wall," + i + " bottles of beer!");
+            System.out.println("take one down, pass it around," + (i-1) + "bottles of beer on the wall!");
+        }
+            
 }
+
+    
