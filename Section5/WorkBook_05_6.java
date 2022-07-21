a47d12a1ebff2a8d74956df21a6de0cc03e9dfa9
Rishab Das <das.rishabk@gmail.com>
2022-07-21 16:05:19 -0400

    added for loop

--- Section5/WorkBook_05_6.java
+++ Section5/WorkBook_05_6.java
@@ -1,23 +1,26 @@
 package Section5;
 
 public class WorkBook_05_6 {
-    // Task 1 – Make a for loop that counts from 0 to 19. 
-    for (int i = 0; i <= 19; i++) {
-        double evenOrNot = i/2;
+    public static void main(String[] args) {
+        // Task 1 – Make a for loop that counts from 0 to 19. 
+        for (int i = 0; i <= 19; i++) {
+            double evenOrNot = i%2;
+            
+            if (evenOrNot > 0) {
+                System.out.println(i + " -odd ");
+            } else {
+                System.out.println(i + " -even ");
+            }
+
 
         
         
 
+        }   
     }
-
-        
-    /* Task 2
-             
-        1. If the number is even, print ' – even' beside the number.
-        2. If the number is odd, print ' – odd' beside the number.
-
-     */
+}    
+    
 
 
 
-}
+
