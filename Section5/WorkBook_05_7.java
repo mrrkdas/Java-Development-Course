23fea002cadc55cbf6b22fde9efba2514f8a5bef
Rishab Das <das.rishabk@gmail.com>
2022-07-21 16:22:26 -0400

    finished workbook_05.7 with help

--- Section5/WorkBook_05_7.java
+++ Section5/WorkBook_05_7.java
@@ -4,14 +4,12 @@ public class WorkBook_05_7 {
     public static void main(String[] args) {
         // Task 1 – Make a for loop that counts from 0 to 18.
         for (int i = 0; i <= 18; i++) {
-            if (i%3 == 0) {
-                System.out.println(i + "--fizz");
-            } else if (i%5 == 0) {
-                System.out.println(i + "--buzz");
-            } else if ((i%3 == 0) && (i%5 == 0)) {
-                System.out.println(i + "--fizzbuzz");
-            } else {
-                System.out.println(i);
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println(i + " FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println(i + " Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println(i + " Buzz");
             }
         }
        
