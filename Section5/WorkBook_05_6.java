d978cc20bf7fb397c4eb8a14ae65b93070d79783
Rishab Das <das.rishabk@gmail.com>
2022-07-21 16:01:03 -0400

    added file and created outline

---
+++ Section5/WorkBook_05_6.java
@@ -0,0 +1,23 @@
+package Section5;
+
+public class WorkBook_05_6 {
+    // Task 1 – Make a for loop that counts from 0 to 19. 
+    for (int i = 0; i <= 19; i++) {
+        double evenOrNot = i/2;
+
+        
+        
+
+    }
+
+        
+    /* Task 2
+             
+        1. If the number is even, print ' – even' beside the number.
+        2. If the number is odd, print ' – odd' beside the number.
+
+     */
+
+
+
+}
