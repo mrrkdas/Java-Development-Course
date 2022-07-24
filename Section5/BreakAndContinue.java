3c9e05255c0f372470d39b137a7f9fc8206fcd5b
Rishab Das <das.rishabk@gmail.com>
2022-07-24 12:59:51 -0400

    ran file

--- Section5/BreakAndContinue.java
+++ Section5/BreakAndContinue.java
@@ -3,6 +3,9 @@ package Section5;
 public class BreakAndContinue {
     public static void main(String[] args) {
         for (int i = 0; i <= 10; i++) {
+            if (i % 2 != 0) {
+                continue;
+            }
             System.out.println(i);
         }
     }
