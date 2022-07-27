317cfeed7350f897553ba66525905557434ab241
Rishab Das <das.rishabk@gmail.com>
2022-07-27 14:56:16 -0400

    finished workbook 6.2

---
+++ Section6/WorkBook_06_2.java
@@ -0,0 +1,13 @@
+package Section6;
+
+public class WorkBook_06_2 {
+    public static void main(String[] args) {
+        String[] students = {"Malfoy", "Crabbe", "Goyle", "Pansy", "Dean"};
+
+        for (int i = 0; i < students.length; i++) {
+            System.out.println(students[i] + " you will take seat " + i);
+        }
+
+        
+    }
+}
