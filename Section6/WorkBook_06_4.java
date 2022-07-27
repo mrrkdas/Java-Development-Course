a00d512bd88d290ebe3e75e036a6b5eaac259991
Rishab Das <das.rishabk@gmail.com>
2022-07-27 15:06:48 -0400

    created file and finished task 1

---
+++ Section6/WorkBook_06_4.java
@@ -0,0 +1,26 @@
+package Section6;
+
+import java.util.Scanner;
+
+public class WorkBook_06_4 {
+    public static void main(String[] args) {
+        
+        String[] store = {"apples", "bananas", "candy", "chocolate", "coffee", "tea"};
+        System.out.println("\nWelcome to Java Grocers. ");
+        System.out.println("What can I help you find?\n");
+        //  Task 1: Set up Scanner, and pick up the user's response.  
+        Scanner scan = new Scanner(System.in);
+
+        String find = scan.nextLine();
+
+        
+        for (int i = 0; i < store.length; i++) {
+          /* Task 2  
+                if element equals user's response  {
+                    println: \nWe have that in aisle:  <index>
+                    break;
+                }
+          */
+        }
+    }
+}
