36b9c89f6b8e3b38c7f3bf6bb73e33e808f8d679
Rishab Das <das.rishabk@gmail.com>
2022-07-27 14:50:53 -0400

    finished lesson

--- Section6/ArraysAndLoops.java
+++ Section6/ArraysAndLoops.java
@@ -3,5 +3,11 @@ package Section6;
 public class ArraysAndLoops {
     public static void main(String[] args) {
         String[] kingdoms = {"Mercia", "Wessex", "Northnumbria", "EA"};
+
+        System.out.println("The number of elements is " + kingdoms.length);
+
+        for (int i = 0; i < kingdoms.length ; i++) {
+            System.out.println("The ellement and index " + i + " is " + kingdoms[i]);
+        }
     }
 }
