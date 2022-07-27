9cdafde1dadc266266285683487b57566849377d
Rishab Das <das.rishabk@gmail.com>
2022-07-27 14:41:21 -0400

    created first java array:

--- Section6/IntroToArrays.java
+++ Section6/IntroToArrays.java
@@ -2,6 +2,10 @@ package Section6;
 
 public class IntroToArrays {
     public static void main(String[] args) {
-        
+        String[] kingdoms = {"Mercia", "Wessex", "Northnumbria", "EA"};
+        System.out.println(kingdoms[0]);
+        System.out.println(kingdoms[1]);
+        System.out.println(kingdoms[2]);
+        System.out.println(kingdoms[4]);
     }
 }
