e78bc52a3c13cb9e03dea28bdf05ff46d758bc4d
Rishab Das <das.rishabk@gmail.com>
2022-07-29 15:38:11 -0400

    finished workbook 6.6

--- Section6/WorkBook_06_6.java
+++ Section6/WorkBook_06_6.java
@@ -11,17 +11,26 @@ public class WorkBook_06_6 {
         }
     
         int highScore = 0;
-        for (int i = 0; i < scores.length; i++) {
-            if (scores[i] > highScore) {
-                highScore = scores[i];
+        int i = 0;
+        for (int i_ = 0; i_ < scores.length; i_++) {
+            if (scores[i_] > highScore) {
+                highScore = scores[i_];
+                i = i_;
             }
+
         }
+
+        System.out.println("The highest score is "  + highScore + " . Impressive");
+        System.out.println("\n\nThe highest score is the gentleman in seat " + i + ". Give that man a cookie!");
+
         /** Task 1 – Find the person with the highest score
          *  
          *   • After obtaining the highest score, print: The highest score is:  <highScore>. Impressive!
          *   • It's the gentleman in seat: <index>. Give that man a cookie!
          *  
          */
+
+
     }
     
     public static int randomNumber() {
