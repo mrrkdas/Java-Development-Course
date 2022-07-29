84ccb4b52707a0df012aa6671aaff16f0c577cd8
Rishab Das <das.rishabk@gmail.com>
2022-07-29 15:40:23 -0400

    created workbook 6.7

---
+++ Section6/WorkBook_06_7.java
@@ -0,0 +1,26 @@
+package Section6;
+
+public class WorkBook_06_7 {
+    public static void main(String[] args) {
+        String[] record = {"WIN", "WIN", "WIN", "LOSS", "WIN", "WIN", "LOSS"};
+
+        /** Task 1 
+         * 
+         *   Using a for loop:
+         *       count Java's wins and store the result in a variable: wins. 
+         *       count Java's losses and store the result in a variable: losses.
+         *
+         * 
+         * 
+         */
+
+        /** Task 2
+         * Print:
+         *     • \nWith a professional record of <wins> Wins and <losses> losses. 
+         *     • He is the pride of oracle: Java Fury!
+         *
+         * 
+         */        
+
+    }
+}
