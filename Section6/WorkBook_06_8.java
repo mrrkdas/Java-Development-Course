603293ebc173a449e965e26949c764287311ab69
Rishab Das <das.rishabk@gmail.com>
2022-07-31 14:39:25 -0400

    finished task 1

--- Section6/WorkBook_06_8.java
+++ Section6/WorkBook_06_8.java
@@ -6,7 +6,9 @@ public class WorkBook_06_8 {
     public static void main(String[] args) {
 
         //Task 1: Create two arrays (see Workbook article)
-
+        String[] apples = {"Gala", "Granny Smith", "Macintosh"};
+        double[] price = {1.99, 1.49, 1.29};
+        
         System.out.println("Here's your receipt:\n");
         /* Task 2
             Use a for loop to print each apple and the corresponding price.
