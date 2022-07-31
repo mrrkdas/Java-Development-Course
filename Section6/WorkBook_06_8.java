e70e199666392fcc4bb6d47df1e9f30e0dcc99fa
Rishab Das <das.rishabk@gmail.com>
2022-07-31 14:37:44 -0400

    created file

---
+++ Section6/WorkBook_06_8.java
@@ -0,0 +1,19 @@
+package Section6;
+
+import java.util.Arrays;
+
+public class WorkBook_06_8 {
+    public static void main(String[] args) {
+
+        //Task 1: Create two arrays (see Workbook article)
+
+        System.out.println("Here's your receipt:\n");
+        /* Task 2
+            Use a for loop to print each apple and the corresponding price.
+            
+            for {
+                println(\t<apple i >: $<price i >)
+            }
+        */
+    }
+}
