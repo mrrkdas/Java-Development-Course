af21e7e0456f096e7501415d8e5d9b7ea1c6b8f2
Rishab Das <das.rishabk@gmail.com>
2022-07-31 14:46:30 -0400

    changed array val

--- Section6/UpdatingArrays.java
+++ Section6/UpdatingArrays.java
@@ -1,7 +1,13 @@
 package Section6;
 
+import java.util.Arrays;
+
 public class UpdatingArrays {
     public static void main(String[] args) {
-        
+        String[] menu = {"espresso", "iced coffee", "macchiato"};
+
+        menu[2] = "Latte";
+
+        System.out.println(Arrays.toString(menu));
     }
 }
