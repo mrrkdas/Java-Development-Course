d6ac5a90a44b23982a2eb89769f2a6311e1acdfe
Rishab Das <das.rishabk@gmail.com>
2022-07-31 14:51:06 -0400

    finished lesson

--- Section6/UpdatingArrays.java
+++ Section6/UpdatingArrays.java
@@ -9,5 +9,15 @@ public class UpdatingArrays {
         menu[2] = "Latte";
 
         System.out.println(Arrays.toString(menu));
+
+        String[] newMenu = new String[5]; // Values are null by default
+
+        for (int i = 0; i < menu.length; i++) {
+            newMenu[i] = menu[i];
+        }
+        newMenu[3] = "House Blend";
+        newMenu[4] = "Dark Roast";
+        System.out.println(Arrays.toString(newMenu));
+
     }
 }
