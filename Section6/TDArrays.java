a9f64bd720cf948548fc4f5f7df0da367b83713a
Rishab Das <das.rishabk@gmail.com>
2022-08-01 15:40:28 -0400

    finished lesson

--- Section6/TDArrays.java
+++ Section6/TDArrays.java
@@ -4,10 +4,23 @@ import java.util.Arrays;
 
 public class TDArrays {
     public static void main(String[] args) {
-        int[][] grades = new int[3][4];
+        int [][] grades = {
+            {72, 74, 78, 76},
+            {65, 64, 61, 67},
+            {95, 98, 99, 100}
+        };
+                                    //3
+        for (int i = 0; i < grades.length; i++) {
 
-        System.out.println(Arrays.toString(grades[0]));
-        System.out.println(Arrays.toString(grades[1]));
-        System.out.println(Arrays.toString(grades[2]));
+            switch (i) {
+                case 0: System.out.print("Harry: "); break;
+                case 1: System.out.print("Ron: "); break;
+                case 2: System.out.print("Hermione: "); break;
+            }
+            for (int j = 0; j < grades[i].length; j++) {
+                System.out.print(grades[i][j] + " ");
+            }
+            System.out.print("\n");
+        }
     }
 }
