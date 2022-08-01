28ff0b620ee5476b98dd72b1f77c7a4931580333
Rishab Das <das.rishabk@gmail.com>
2022-08-01 15:05:38 -0400

    finished lesson

--- Section6/RefrenceTrap.java
+++ Section6/RefrenceTrap.java
@@ -1,7 +1,15 @@
 package Section6;
 
+import java.util.Arrays;
+
 public class RefrenceTrap {
     public static void main(String[] args) {
-        
+        String[] staffLastYear = {"Tommy", "Joel", "Ellie"};
+        String[] staffThisYear = Arrays.copyOf(staffLastYear, staffLastYear.length);
+
+        staffThisYear[1] = "Abby";
+
+        System.out.println(Arrays.toString(staffLastYear));
+        System.out.println(Arrays.toString(staffThisYear));
     }
 }
