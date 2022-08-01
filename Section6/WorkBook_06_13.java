454028981213d508ac4fad528ebc48cb3d9b0ee5
Rishab Das <das.rishabk@gmail.com>
2022-08-01 15:45:55 -0400

    finished task 3

--- Section6/WorkBook_06_13.java
+++ Section6/WorkBook_06_13.java
@@ -28,5 +28,17 @@ public class WorkBook_06_13 {
 
         // Task 3 - Print the prices for each department. See the article for the expected output.  
 
+        for (int i = 0; i < prices.length; i++) {
+            switch (i) {
+                case 0: System.out.print("Baking: "); break;
+                case 1: System.out.print("Beverage: "); break;
+                case 2: System.out.print("Cereals: "); break;
+            }
+            for (int j = 0; j < prices[i].length; j++) {
+                System.out.print(prices[i][j] + " ");
+            }
+            System.out.print("\n");
+        }
+
     }
 }
