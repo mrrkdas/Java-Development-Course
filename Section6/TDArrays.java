02cad918a124c7f81a49172ae9768fd688ef43b1
Rishab Das <das.rishabk@gmail.com>
2022-08-01 15:37:13 -0400

    added arrays

--- Section6/TDArrays.java
+++ Section6/TDArrays.java
@@ -1,7 +1,13 @@
 package Section6;
 
+import java.util.Arrays;
+
 public class TDArrays {
     public static void main(String[] args) {
-        
+        int[][] grades = new int[3][4];
+
+        System.out.println(Arrays.toString(grades[0]));
+        System.out.println(Arrays.toString(grades[1]));
+        System.out.println(Arrays.toString(grades[2]));
     }
 }
